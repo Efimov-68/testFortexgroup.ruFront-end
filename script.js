672c1dc879b0f18c79bcc6c4d691972ffaa928b0
Roman <r.efimov@list.ru>
2017-08-10 14:07:12 +0300

    submit style red

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
   $('td input[type="checkbox"]').hide();
 
   //end hide()
+
+  //input show() - hide()
   $('#allcontents input[name="allcontents"]').click(function(){
     if ($('#allcontents input[name="allcontents"]').prop('checked') == true) {
       $('td input[type="checkbox"]').show();
     }else{
       $('td input[type="checkbox"]').hide();
     }
-  });
+  })//end input show() - hide()
 
   //Allcontents checked
   $('#allcontents input[name="allcontents"]').click(function(){
